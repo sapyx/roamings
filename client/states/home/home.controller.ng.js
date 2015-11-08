555e9a10b6b4a9685558c39546d7e41ed9fd7d35
Sapyx <sapyx@aruba.it>
2015-11-08 01:06:09 +0100

    use config Collection

--- client/states/home/home.controller.ng.js
+++ client/states/home/home.controller.ng.js
@@ -3,12 +3,19 @@
 /**
  * @ngdoc controller
  * @name roamingsApp.controller:MainCtrl
- * @requires
+ * @requires $meteor
  * @description
  * # MainCtrl
  * Controller of the roamingsApp
  */
 angular.module('roamingsApp')
-    .controller('HomeCtrl', function () {
-      this.corporation ={Id:98361290, Name: 'Deus-Ex-Machina', Ticker: 'D-E-X'};
+    .controller('HomeCtrl', function ($meteor) {
+        var self = this;
+
+        $meteor.subscribe('configs').then(function (subscriptionHandle) {
+            self.corporation = $meteor.collection(Configs, false, false)[0];
+
+            // You can use the subscription handle to stop the subscription if you want
+            subscriptionHandle.stop();
+        });
     });
