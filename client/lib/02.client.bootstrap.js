917940de17954cae1ea9caac5e3a9f757aa14bdb
Sapyx <sapyx@aruba.it>
2015-12-10 22:50:50 +0100

    implement splash screen using  meteorhacks:inject-initial

--- client/lib/02.client.bootstrap.js
+++ client/lib/02.client.bootstrap.js
@@ -1,5 +1,22 @@
+Meteor.startup(function() {
+    var $head, $tags, tag, _i, _len, _ref, _results;
+    $head = $('head');
+    _ref = ['meta', 'title'];
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        tag = _ref[_i];
+        $tags = $(tag);
+        $head.append($tags.clone());
+        _results.push($tags.remove());
+    }
+    return _results;
+});
+
+
 onReady = function () {
     angular.bootstrap(angular.element("html")[0], ['roamingsApp']); //Full jQuery only
+    angular.element(".spinner").hide();
+    angular.element(".banner").hide();
 };
 
 if (Meteor.isCordova) {
