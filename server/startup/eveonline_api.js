c2d466c20af76bba3c88731e6daf2910587ec383
Sapyx <sapyx@aruba.it>
2015-12-15 02:01:35 +0100

    eve api server side

--- server/startup/eveonline_api.js
+++ server/startup/eveonline_api.js
@@ -2,14 +2,35 @@
  * Created by Sapyx on 13/12/2015.
  */
 
-Meteor.startup(function() {
+Meteor.startup(function () {
     console.log('enter API function');
+
+    HTTP.get('https://api.eveonline.com/eve/CharacterAffiliation.xml.aspx?ids=905954997', {},
+        (xmlError, xmlResponse) => {
+            if (xmlError) {
+                console.error('xmlError', xmlError);
+            } else {
+                xml2js.parseString(xmlResponse.content, {
+                    explicitArray: false,
+                    emptyTag: undefined
+                }, (jsError, jsResult)=> {
+                    if (jsError) {
+                        console.error('xml2js error', jsError);
+                    } else {
+                        console.dir(jsResult.eveapi.result.rowset.row.$.characterName);
+                    }
+                });
+            }
+        }
+    );
+    // -------------------------
+/*
 
     try {
         var response = HTTP.get('https://api.eveonline.com/eve/CharacterAffiliation.xml.aspx?ids=905954997');
     }
     catch (error) {
-        console.dir( error.response);
+        console.dir(error.response);
         response = error.response;
     }
 
@@ -21,7 +42,7 @@ Meteor.startup(function() {
 
     if (response.statusCode !== 200) {
         var reason = 'API endpoint did not specify error details.';
-        /*   if (response.content) {
+        /!*   if (response.content) {
          try {
          obj = (blocking(this.parser, this.parser.parseString))(response.content);
          reason = obj.eveapi.error._;
@@ -31,9 +52,10 @@ Meteor.startup(function() {
          error = _error;
          reason = response.content;
          }
-         }*/
+         }*!/
         //throw new Meteor.Error(response.statusCode, reason);
     }
+*/
 
     //return response.content;
 });
